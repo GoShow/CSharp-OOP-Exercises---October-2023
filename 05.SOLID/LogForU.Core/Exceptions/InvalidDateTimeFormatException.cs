53af4e9a0b000cb05a4840611ab82d18e28b6d88
Georgi Inkov <georgi.inkov@scaleforce.io>
2023-11-13 22:59:21 +0200

    SOLID

---
+++ 05.SOLID/LogForU.Core/Exceptions/InvalidDateTimeFormatException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace LogForU.Core.Exceptions;
+
+public class InvalidDateTimeFormatException : Exception
+{
+    private const string DefaultMessage =
+        "Invalid DateTime format";
+
+    public InvalidDateTimeFormatException()
+        : base(DefaultMessage)
+    {
+    }
+
+    public InvalidDateTimeFormatException(string message)
+        : base(message)
+    {
+    }
+}
