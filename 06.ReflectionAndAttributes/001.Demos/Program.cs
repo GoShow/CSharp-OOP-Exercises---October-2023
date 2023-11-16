b3492277865753daa3f36eade7efa001be041cb2
Georgi Inkov <georgi.inkov@scaleforce.io>
2023-11-16 21:43:52 +0200

    Reflection

---
+++ 06.ReflectionAndAttributes/001.Demos/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Reflection;
+
+Type type = typeof(Person);
+
+PropertyInfo propertyInfo = type.GetProperty("Age");
+MethodInfo methodInfo = type.GetMethod("GetName");
+FieldInfo fieldInfo = type.GetField("name", BindingFlags.Instance | BindingFlags.NonPublic);
+MemberInfo[] memberInfos = type.GetMember("GetName");// check how to invoke
+
+Person instance = Activator.CreateInstance(type, new object[] { "Ivan", 18 }) as Person;
+//Person instance = Activator.CreateInstance(type, "Ivan", 18) as Person;
+
+Console.WriteLine(instance.GetName());
+
+fieldInfo.SetValue(instance, "Andrey");
+
+Console.WriteLine(instance.GetName());
+
+object result = methodInfo.Invoke(instance, null);
+
+Console.WriteLine(result);
+
+public class Person
+{
+    private string name;
+
+    public Person(string name, int age)
+    {
+        this.name = name;
+        Age = age;
+    }
+
+    public int Age { get; set; }
+
+    public string GetName() => name;
+
+    //public string GetName(string fullName) => fullName;  //Use for GetMember
+
+}
---
+++ 06.ReflectionAndAttributes/02.ValidationAttributes/StartUp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using ValidationAttributes.Models;
+using ValidationAttributes.Utils;
+
+namespace ValidationAttributes;
+
+public class StartUp
+{
+    public static void Main(string[] args)
+    {
+        Person person = new(null, -1);
+
+        Console.WriteLine(Validator.IsValid(person));
+    }
+}
