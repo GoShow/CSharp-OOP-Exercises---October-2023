8711cb302588531df90302d0e0945d0bb1e2dcd1
Georgi Inkov <georgi.inkov@scaleforce.io>
2023-11-27 21:25:01 +0200

    Design patterns

---
+++ 09.DesignPatterns/01.Prototype/Models/Sandwich.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Prototype.Models;
+
+//[Serializable]
+public class Sandwich : SandwichPrototype
+{
+    private string bread;
+    private string meat;
+    private string cheese;
+    private string veggies;
+
+    public Sandwich(string bread, string meat, string cheese, string veggies)
+    {
+        this.bread = bread;
+        this.meat = meat;
+        this.cheese = cheese;
+        this.veggies = veggies;
+        //this.Prices = new();
+    }
+
+    //public List<decimal> Prices { get; set; }
+
+    public override SandwichPrototype Clone()
+    {
+        Console.WriteLine($"Cloning sandwich with ingredients: {GetIngredients()}");
+
+        return MemberwiseClone() as SandwichPrototype;
+    }
+
+    //public override SandwichPrototype DeepClone()
+    //{
+    //    Console.WriteLine($"Cloning sandwich with ingredients: {GetIngredients()}");
+
+    //    using (var ms = new MemoryStream())
+    //    {
+    //        var formatter = new BinaryFormatter();
+    //        formatter.Serialize(ms, this);
+    //        ms.Position = 0;
+
+    //        return formatter.Deserialize(ms) as SandwichPrototype;
+    //    }
+    //}
+
+    private string GetIngredients()
+        => $"{bread}, {meat}, {cheese}, {veggies}";
+}
---
+++ 09.DesignPatterns/02.Composite/Models/SingleGift.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Composite.Models;
+
+public class SingleGift : GiftBase
+{
+    public SingleGift(string name, decimal price)
+        : base(name, price)
+    {
+    }
+
+    public override decimal CalculateTotalPrice()
+    {
+        Console.WriteLine($"{Name} with the price {Price}");
+
+        return Price;
+    }
+}
